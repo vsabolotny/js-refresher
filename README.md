23d6be42ec97643204d42c0169a5018790da1d4c
Vladislav Sabolotny <vladislav.sabolotny@gmail.com>
2021-05-14 10:08:00 +0200

    Handle async and promise

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # JS Refresher
 This is a collection of some js play around scripts. 
+
+Install node.js first and then just run
+```
+$ node play.js
+```
--- play.js
+++ play.js
@@ -1,5 +1,7 @@
 
-const myname = "Vlad";
+/** Variable & Function */
+
+let myname = "Vlad";
 
 const greetingUser = (name) => {
     return "hello " + name + "!";
@@ -12,7 +14,7 @@ console.log(greetingUser(myname));
 console.log(addOne(1));
 console.log(add(2,1));
 
-/** --- */
+/** Object */
 
 const person = {
     name: "Vlad",
@@ -25,7 +27,7 @@ const person = {
 console.log(person);
 person.greet();
 
-/** --- */
+/** Array */
 
 const hobbies = ["cook", "hiking"];
 console.log(hobbies.map(hobby => "Hobby: " + hobby));
@@ -33,7 +35,54 @@ console.log(hobbies.map(hobby => "Hobby: " + hobby));
 hobbies.push("programming");
 console.log(hobbies);
 
-/** --- */
+/** Rest & Spread */
 
 const hobbiesExt = [...hobbies];
 console.log("Hobbies extended: " + hobbiesExt);
+
+const toArray = (...args) => {
+    return [...args];
+};
+console.log(toArray(1,2,3,4,5));
+
+/** Destructuring */
+
+const employee = {
+    name: "Vlad",
+    age: 29,
+    city: "Munich",
+    greet() {
+        console.log("Hi, welcome to the js refresher, " + this.name);
+    }
+}
+
+const printName = ({ name, age }) => {
+    console.log(name + " is " + age + " years old");
+}
+printName(employee);
+
+const { name, age, city } = employee;
+console.log(name, age, city);
+
+const [h1, h2] = hobbies;
+console.log(h1, h2);
+
+/** Async & Promises */
+
+const fetchData = () => {
+    const promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("Done callback call");
+        }, 1500);
+    });
+    return promise;
+}
+
+setTimeout(() => {
+    console.log("Timer is done");
+    fetchData().then(text => {
+        console.log(text);
+        return fetchData();
+    });
+}, 2000);
+
